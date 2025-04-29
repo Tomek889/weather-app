8015246ad09169255e373d68ceef7db3e771af08
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-04-29 17:32:07 +0200

    Create a function getForecast to fetch data from the Weather API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+// eslint-disable-next-line consistent-return
+async function getForecast(location) {
+  try {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=BC6528TYCKWQREPG7JR8BLLCW&contentType=json`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getForecast('cracow').then((forecast) => {
+  console.log(forecast);
+});
